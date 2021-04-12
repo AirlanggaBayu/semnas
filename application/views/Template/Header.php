b6a40d380b375fc159e9f92ec7e6b6d23f72b94c
dodhyk <50976164+dodhyk@users.noreply.github.com>
2021-04-12 10:30:47 +0700

    Benahi Tampilan
    Menambahkan fitur tampilan sesuai dengan yang diminta, masih kurang artikel dan template

--- application/views/Template/Header.php
+++ application/views/Template/Header.php
@@ -28,3 +28,33 @@
     <link href="<?= base_url('assets/public/assets/css/theme.css'); ?>" rel="stylesheet" />
 
 </head>
+
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+<body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 backdrop" data-navbar-on-scroll="data-navbar-on-scroll">
+            <div class="container"><a class="navbar-brand d-flex align-items-center fw-bolder fs-2 fst-italic" href="#">
+                    <div class="text-info">JO</div>
+                    <div class="text-warning">BEST</div>
+                </a>
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto pt-2 pt-lg-0">
+                        <li class="nav-item px-2"><a class="nav-link fw-medium" aria-current="page" href="#">Home</a></li>
+                        <li class="nav-item px-2"><a class="nav-link fw-medium" href="#tentang">Tentang Kami</a></li>
+                        <li class="nav-item px-2"><a class="nav-link fw-medium" href="#jadwal">Jadwal</a></li>
+                        <li class="nav-item px-2"><a class="nav-link fw-medium" href="#pembicara">Pembicara</a></li>
+                        <li class="nav-item px-2"><a class="nav-link fw-medium" href="#template">Template</a></li>
+                        <li class="nav-item px-2"><a class="nav-link fw-medium" href="#artikel">Artikel</a></li>
+                    </ul>
+                    <form class="ps-lg-5">
+                        <button class="btn btn-lg btn-primary rounded-pill bg-gradient order-0" type="submit">Sign In</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
